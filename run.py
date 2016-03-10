d4e775e7c71e3a22806a689df494cfe6e6d067ec
Cawb07 <Cawb07@users.noreply.github.com>
2016-03-10 18:38:58 -0500

    Revert "Ap front"

--- run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from template_library import app
-
-if __name__ == '__main__':
-	app.run(debug=True)
--- template_library/terminalone/metadata.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Prasanna Swaminathan'
-__copyright__ = 'Copyright 2015, MediaMath'
-__license__ = 'BSD'
-__version__ = '1.1.0'
-__maintainer__ = 'MediaMath Developer Relations'
-__email__ = 'developers@mediamath.com'
-__status__ = 'Stable'
